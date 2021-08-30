d800055c2e4194dc76e5f8fde0beb065dcddba87
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-31 01:43:18 +0530

    Create WeekDay.js

---
+++ WeekDay.js
@@ -0,0 +1,32 @@
+//For promting message and taking input from console.
+const prompt = require("prompt-sync")();
+
+let userNumber = prompt("Enter number between 1 to 7 : ");
+
+let number = +userNumber;
+
+switch (number) {
+  case 1:
+    console.log("Today is Monday.");
+    break;
+  case 2:
+    console.log("Today is Tuesday.");
+    break;
+  case 3:
+    console.log("Today is Wednesday.");
+    break;
+  case 4:
+    console.log("Today is Thursday.");
+    break;
+  case 5:
+    console.log("Today is Friday.");
+    break;
+  case 6:
+    console.log("Today is Saturday.");
+    break;
+  case 7:
+    console.log("Today is Sunday.");
+    break;
+  default:
+    console.log("Wrong input");
+}
