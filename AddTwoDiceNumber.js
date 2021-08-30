f373dd1a6131d2acc41fea2b84cd33b34e12843e
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-31 01:20:51 +0530

    Create AddTwoDiceNumber.js

---
+++ AddTwoDiceNumber.js
@@ -0,0 +1,3 @@
+let firstDice = Math.floor(Math.random() * (6 - 1)) + 1;
+let secondDice = Math.floor(Math.random() * (6 - 1)) + 1;
+console.log("Additin of two dice number is : " + firstDice + secondDice);
