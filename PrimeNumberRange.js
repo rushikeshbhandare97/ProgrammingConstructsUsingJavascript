0cfbe6f8342ef6dcc3f75c8f1b1824e1c986578e
rushikeshbhandare <86002633+rushikeshbhandare97@users.noreply.github.com>
2021-08-31 01:52:30 +0530

    Create PrimeNumberRange.js

---
+++ PrimeNumberRange.js
@@ -0,0 +1,28 @@
+//For promting message and taking input from console.
+const prompt = require("prompt-sync")();
+
+// take input from the user
+const lowerNumber = parseInt(prompt("Enter lower number: "));
+const higherNumber = parseInt(prompt("Enter higher number: "));
+
+console.log(
+  `The prime numbers between ${lowerNumber} and ${higherNumber} are:`
+);
+
+// looping from lowerNumber to higherNumber
+for (let i = lowerNumber; i <= higherNumber; i++) {
+  let flag = 0;
+
+  // looping through 2 to user input number
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      flag = 1;
+      break;
+    }
+  }
+
+  // if number greater than 1 and not divisible by other numbers
+  if (i > 1 && flag == 0) {
+    console.log(i);
+  }
+}
